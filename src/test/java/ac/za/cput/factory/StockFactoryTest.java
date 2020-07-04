0a10113d1940de79c32dfac9da76813d043b6c0d
Sherwin <originsherwin@gmail.com>
2020-07-04 16:18:52 +0200

    refactored code

--- src/test/java/ac/za/cput/factory/StockFactoryTest.java
+++ src/test/java/ac/za/cput/factory/StockFactoryTest.java
@@ -3,8 +3,6 @@ package ac.za.cput.factory;
 import ac.za.cput.entity.Stock;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 /*
  *  @author: Sherwin Adams
  *  Desc: Tests for Stock Factory
@@ -13,15 +11,8 @@ import static org.junit.Assert.*;
 public class StockFactoryTest {
 
     @Test
-    public void createFurnitureStock() {
-        Stock furniture = StockFactory.createFurnitureStock(20, "02-06-2020");
+    public void createStock() {
+        Stock furniture = StockFactory.createStock(20, "02-06-2020");
         System.out.println(furniture);
-    }
-
-    @Test
-    public void createDeviceStock() {
-        Stock device = StockFactory.createDeviceStock(10,"02-07-2020");
-        System.out.println(device);
-
     }
 }
