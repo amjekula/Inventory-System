514e434bcb7d06f853d434b6b94c91347d160e7d
Joselle Dinana <joselledina@gmail.com>
2020-08-28 19:35:01 +0200

    created repository package

--- src/main/java/ac/za/cput/repository/IRepository.java
+++ src/main/java/ac/za/cput/repository/IRepository.java
@@ -1,12 +1,11 @@
 package ac.za.cput.repository;
 
-import ac.za.cput.entity.user.Login;
 
 public interface IRepository<T, ID> {
 
-    Login create(T t);
-    Login read(ID id);
-    Login update(T t);
+     T create(T t);
+     T read(ID id);
+     T update(T t);
     void delete(ID id);
 
 }
