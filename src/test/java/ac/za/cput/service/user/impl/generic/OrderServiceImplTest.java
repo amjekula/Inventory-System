4a2f81e20b10b4c052d6941126b0b34a32167e1a
Lance <lancejohnston3@gmail.com>
2020-09-05 20:29:59 +0200

    first commit

--- src/test/java/ac/za/cput/service/user/impl/generic/OrderServiceImplTest.java
+++ src/test/java/ac/za/cput/service/user/impl/generic/OrderServiceImplTest.java
@@ -3,11 +3,8 @@ package ac.za.cput.service.user.impl.generic;
 
 import ac.za.cput.entity.generic.Order;
 import ac.za.cput.factory.generic.OrderFactory;
-import ac.za.cput.repository.generic.OrderRepository;
-import ac.za.cput.repository.generic.impl.OrderRepositoryImpl;
 import ac.za.cput.service.generic.OrderService;
 import ac.za.cput.service.generic.impl.OrderServiceImpl;
-import javafx.scene.effect.SepiaTone;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
@@ -17,53 +14,51 @@ import java.util.Set;
 import static org.junit.Assert.*;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class OrderServiceImplTest {
+public class OrderServiceImplTest
+{
     private static OrderService orderService = OrderServiceImpl.getService();
-    private static Order orderBuilder = OrderFactory.createClerkOrder("test", "9/04/2020");
+    private static Order orderBuilder = OrderFactory.createStockOrder("test", "5/09/2020");
 
+    @Test
+    public void d_getAll()
+    {
+        Set<Order> orderSet = orderService.getAll();
+        assertEquals(1, orderSet.size());
+        System.out.println("All Users: " + orderService.getAll() + "\n");
+    }
 
+    @Test
     public void a_create()
     {
-        Order created = orderService.create(orderBuilder);
-        assertEquals(orderBuilder.getStockId(), created.getStockId());
-        System.out.print("created order: " + " " + created + "\n");
-        System.out.print("\n");
+        Order createOrder = orderService.create(orderBuilder);
+        assertEquals(createOrder.getDescription(), createOrder.getDescription());
+        System.out.println("Create: " + createOrder + "\n");
     }
 
     @Test
     public void b_read()
     {
-        Order read = orderService.read(orderBuilder.getStockId());
-        assertEquals(orderBuilder.getStockId(), read.getStockId());
-        System.out.print("reading order: " + " " + read);
-        System.out.print("\n");
+        Order readOrder = orderService.read(orderBuilder.getDescription());
+        assertEquals(orderBuilder.getDescription(), orderBuilder.getDescription());
+        System.out.println("Read: " + readOrder + "\n");
     }
 
     @Test
     public void c_update()
     {
-        Order updated = new Order.Builder().copy(orderBuilder).setDescription("test2").setOrderNum("123456").build();
-        updated = orderService.update(updated);
-        assertEquals(orderBuilder.getStockId(), updated.getStockId());
-        assertNotEquals(orderBuilder.getDescription(), updated.getDescription());
-        assertNotEquals(orderBuilder.getOrderNum(), updated.getOrderNum());
-        System.out.print("\n"+" updated order:" + " " + updated +"\n");
-
+        Order updateOrder = new Order.Builder().copy(orderBuilder).setDescription("test2").build();
+        updateOrder = orderService.update(updateOrder);
+        assertNotEquals(orderBuilder.getDescription(), updateOrder.getDescription());
+        System.out.println("Update: " + updateOrder + "\n");
     }
 
     @Test
     public void e_delete()
     {
-        orderService.delete(orderBuilder.getStockId());
-        assertEquals(0, orderService.getAll().size());
-        System.out.print("\n" + "id removed:" + "  " + orderBuilder.getStockId() + "\n");
-        System.out.print("\n" + orderService.getAll());
-    }
-    @Test
-    public void d_getAll()
-    {
         orderService.delete(orderBuilder.getClerkId());
-        assertEquals(orderService.getAll().size(), 0);
+        assertEquals(orderService.getAll().size(), 1);
         System.out.println("Check if Object is deleted: " + orderService.getAll());
     }
+
+
 }
