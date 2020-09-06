0b79395ccda867b554d2112a3b5790894199c306
Sherwin <originsherwin@gmail.com>
2020-09-06 04:05:17 +0200

    added comments

--- src/main/java/ac/za/cput/service/generic/UniversityService.java
+++ src/main/java/ac/za/cput/service/generic/UniversityService.java
@@ -5,6 +5,11 @@ import ac.za.cput.service.IService;
 
 import java.util.Set;
 
+/*
+ *  @author: Sherwin Adams (216177499)
+ *  Desc: University service
+ */
+
 public interface UniversityService extends IService<University, String> {
     Set<University> getAll();
 }
