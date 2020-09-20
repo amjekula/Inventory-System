5229ff83a4156fe8bfcfdeffa1f7f8be1fffe5f0
Joselle Dinana <joselledina@gmail.com>
2020-09-20 14:36:56 +0200

    created sprintBoot and controller

--- src/main/java/ac/za/cput/controller/HomeController.java
+++ src/main/java/ac/za/cput/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    @RequestMapping({"/","/home"})
+    @RequestMapping({"/","/index/home"})
     String home(){
         return "Inventory System Application";
     }
--- src/main/java/ac/za/cput/controller/user/LoginController.java
+++ src/main/java/ac/za/cput/controller/user/LoginController.java
@@ -2,18 +2,35 @@ package ac.za.cput.controller.user;
 
 
 import ac.za.cput.entity.user.Login;
+import ac.za.cput.factory.user.LoginFactory;
+import ac.za.cput.service.user.impl.LoginServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/login")
 public class LoginController {
+    @Autowired
+    private LoginServiceImpl loginService;
 
 //    @RequestMapping(value = "/create", method = RequestMethod.)
     @PostMapping("/create")
-    public String create(){
-        return "hi";
+    public Login create(@RequestBody Login login) {
+
+        Login newLogin = LoginFactory.createLogin(login.getEmailAddress(), login.getPassword());
+        return loginService.create(newLogin);
+    }
+
+    @GetMapping("/all")
+    public Set<Login> getAll(){
 
 
+        return loginService.getAll();
     }
 
 }
