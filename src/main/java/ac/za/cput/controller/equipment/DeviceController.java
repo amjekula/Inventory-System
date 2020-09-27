3c06c7309e3ba898852b71645f0319bfe1e6cffd
emile.lubangi <Emile.lubangii@gmail.com>
2020-09-28 00:38:58 +0200

    Update before test

--- src/main/java/ac/za/cput/controller/equipment/DeviceController.java
+++ src/main/java/ac/za/cput/controller/equipment/DeviceController.java
@@ -1,16 +1,17 @@
 package ac.za.cput.controller.equipment;
 
 import ac.za.cput.entity.equipment.Device;
+import ac.za.cput.entity.equipment.Furniture;
 import ac.za.cput.factory.equipment.DeviceFactory;
 import ac.za.cput.service.equipment.impl.DeviceServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
+@RestController
+@RequestMapping("device")
 public class DeviceController {
 
     @Autowired
@@ -18,35 +19,29 @@ public class DeviceController {
 
 
     @PostMapping("create")
-
     public Device create(@RequestBody Device device) {
         Device device1 = DeviceFactory.createDevice(device.getDeviceTypeId());
 
         return deviceService.create(device1);
     }
 
-    @PostMapping("delete")
-
-    public Device delete(@RequestBody Device device) {
-
-        return deviceService.delete(String.valueOf(device));
-    }
 
     @GetMapping("read")
-
     public Device read(@RequestParam("id")  String id) {
         return deviceService.read(id);
     }
 
-    @PostMapping("update")
 
+    @PostMapping("update")
     public Device update(@RequestBody Device device) {
         return deviceService.update(device);
     }
 
-    @GetMapping("reads")
 
-    public List<Device> readAll() {
-        return deviceService.readAll();
+
+    @GetMapping("all")
+    public Set<Device> getAll() {
+
+        return deviceService.getAll();
     }
 }
