8a8c81a4e6d6863d9dae69c9822961843fc5bcf3
emile.lubangi <Emile.lubangii@gmail.com>
2020-10-28 11:46:31 +0200

    SAl u[[

--- src/main/java/ac/za/cput/repository/equipment/FurnitureRepository.java
+++ src/main/java/ac/za/cput/repository/equipment/FurnitureRepository.java
@@ -2,10 +2,14 @@ package ac.za.cput.repository.equipment;
 
 import ac.za.cput.entity.equipment.Furniture;
 import ac.za.cput.repository.IRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 
-public interface FurnitureRepository extends IRepository<Furniture, String>
-{
-    Set<Furniture> getAll();
+@Repository
+public interface FurnitureRepository extends JpaRepository <Furniture, String> {
+
+
+
 }
